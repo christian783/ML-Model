18a559a0e93ec4acbea8ed0e84f84b718a446253
christian783 <christiantc0000@gmail.com>
2024-10-01 06:29:55 +0200

    initial commit

---
+++ Model/Program.cs
@@ -0,0 +1,14 @@
+﻿using Model;
+
+// Add input data
+var sampleData = new Sentiment_model.ModelInput()
+{
+    Col0 = "This restaurant was disgusting."
+};
+
+// Load model and predict output of sample data
+var result = Sentiment_model.Predict(sampleData);
+
+// If Prediction is 1, sentiment is "Positive"; otherwise, sentiment is "Negative"
+var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
